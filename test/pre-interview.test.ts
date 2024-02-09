cd654abe26db0f209ee499ac616d02d638fad5ae
iamdhrey <damilare.nelson@outlook.com>
2024-02-09 07:53:06 -0700

    Initial commit

---
+++ test/pre-interview.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PreInterview from '../lib/pre-interview-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/pre-interview-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PreInterview.PreInterviewStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
